eabbbe7da28d25b29d4a47f3e52da23fd4ec16e8
e <wangi0214@163.com>
2018-09-10 13:16:37 +0800

    test

---
+++ src/main/java/com/annoroad/cloud/c/server/AnnoroadCloudCServerApplication.java
@@ -0,0 +1,12 @@
+package com.annoroad.cloud.c.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnoroadCloudCServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnoroadCloudCServerApplication.class, args);
+	}
+}
